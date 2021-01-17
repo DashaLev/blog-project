a9caebdd41c36cac3e4210fc923e99d8dcf2ff6f
Dasha <d.l92@ukr.net>
2021-01-17 23:30:54 +0200

    added js

---
+++ app.js
@@ -0,0 +1,28 @@
+// mobile menu
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu");
+
+mobileMenu.addEventListener("click",function(){
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+})
+
+// contact form
+// mobile menu
+let formFeedback = document.querySelector(".feedback");
+let openFormBtn = document.querySelector(".open-form-btn");
+
+openFormBtn.addEventListener("click",function(){
+    formFeedback.classList.toggle("show");
+    if(formFeedback.classList.contains("show")) {
+        openFormBtn.classList.add("show-button");
+        openFormBtn.textContent = openFormBtn.textContent = "CLOSE FORM";
+    } else {
+        openFormBtn.classList.remove("show-button");
+        openFormBtn.textContent = openFormBtn.textContent = "OPEN FORM";
+    }
+})
